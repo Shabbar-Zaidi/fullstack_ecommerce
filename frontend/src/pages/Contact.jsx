3aa1c4fb39fa337f5ce6553211e128663b47de80
Shabbar Zaidi <shabbar5zaidi@gmail.com>
2025-09-21 17:07:01 +0500

    commit2nd

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Title from "../components/Title";
 import { assets } from "../assets/assets";
-import NewsletterBox from "../components/NewsletterBox";
+import NewsLetterBox from "../components/NewsLetterBox";
 
 const Contact = () => {
   return (
@@ -28,7 +28,7 @@ const Contact = () => {
           <button className="border border-black px-8 py-4 text-sm hover:bg-black hover:text-white">Explore Jobs</button>
         </div>
       </div>
-      <NewsletterBox />
+      <NewsLetterBox />
     </>
   );
 };
